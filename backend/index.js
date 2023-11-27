7252b8e461e7f1ce45bb9e6bee20dbd95171465b
priyaa <priyaa@gmail.com>
2023-11-27 14:47:10 +0530

    uri upted

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv")
 // const bodyParser = require("body-parser")
 const app = express()
 const Routes = require("./routes/route.js")
-
+const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT || 5000
 
 dotenv.config();
@@ -15,13 +15,15 @@ dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
-
-mongoose
-    .connect(process.env.MONGO_URL, {
+// const client = new MongoClient('mongodb://0.0.0.0:27017/?authMechanism=DEFAULT');
+const client = new MongoClient('mongodb+srv://baispriti4:priti@cluster0.8wkbksc.mongodb.net/?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://0.0.0.0:27017/?authMechanism=DEFAULT'
+    mongoose.connect('mongodb+srv://baispriti4:priti@cluster0.8wkbksc.mongodb.net/?retryWrites=true&w=majority'
+        , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Connected to MongoDB"))
+    .then(()=>{console.log("Connected to MongoDB")})
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
 app.use('/', Routes);
