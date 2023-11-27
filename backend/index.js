afb3a242c99158b648589fef166f7ed06673f19f
priyaa <priyaa@gmail.com>
2023-11-27 15:43:42 +0530

    uri upted

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,11 @@ app.use(cors())
 const client = new MongoClient('mongodb+srv://baispriti4:priti@cluster0.8wkbksc.mongodb.net/?retryWrites=true&w=majority')
 // mongoose.connect('mongodb://0.0.0.0:27017/?authMechanism=DEFAULT'
 client.connect('mongodb+srv://baispriti4:priti@cluster0.8wkbksc.mongodb.net/?retryWrites=true&w=majority'
-        , {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    //     , {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true
+    // }
+    )
     .then(()=>{console.log("Connected to MongoDB")})
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
