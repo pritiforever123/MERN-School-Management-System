38ee0a36e3d74a475f426e8ba2024cb85169a305
priyaa <priyaa@gmail.com>
2023-11-29 13:42:44 +0530

    mongoos installed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 // const bodyParser = require("body-parser")
 const app = express()
